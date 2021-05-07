386879ccac7bc346456937b6d16b7ef7941921e9
Serhii Stanislav <serhiistanislav@gmail.com>
2021-05-07 13:06:30 +0300

    adds a removeContact

--- model/index.js
+++ model/index.js
@@ -6,7 +6,10 @@ const contactsPath = path.join('./model/contacts.json');
 // GET
 const listContacts = async () => {
   try {
-    return await fs.readFile(contactsPath, 'utf-8');
+    const response = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(response);
+
+    return { status: 'success', code: 200, contacts };
   } catch (error) {
     console.log(error.message);
   }
@@ -15,36 +18,53 @@ const listContacts = async () => {
 // GET by ID
 const getContactById = async contactId => {
   try {
-    const contacts = await listContacts();
-
-    return await JSON.parse(contacts).find(
+    const { contacts } = await listContacts();
+    const contact = await contacts.find(
       contact => contact.id.toString() === contactId,
     );
+
+    return contact
+      ? { status: 'success', code: 200, contact }
+      : { status: 'error', code: 404, message: 'Not found' };
   } catch (error) {
     console.log(error.message);
   }
 };
 
+// ADD
 const addContact = async body => {
-  try {
-    const newContacts = JSON.stringify([...contacts, body], null, 2);
-    await fs.writeFile(contactsPath, newContacts);
-    return body;
-  } catch (error) {
-    console.log(error.message);
+  const values = Object.values(body).filter(value => value !== '');
+
+  if (values.length !== 4) {
+    return {
+      status: 'error',
+      code: 400,
+      message: 'missing required name field',
+    };
+  } else {
+    try {
+      const newContacts = JSON.stringify([...contacts, body], null, 2);
+      await fs.writeFile(contactsPath, newContacts);
+
+      return { status: 'success', code: 201, contact: body };
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 };
 
+// DEL
 const removeContact = async contactId => {
   try {
-    const contacts = await listContacts();
+    const { contacts } = await listContacts();
 
-    const filteredContacts = await JSON.parse(contacts).filter(contact => {
-      return contact.id.toString() !== contactId;
-    });
+    const filteredContacts = await contacts.filter(
+      contact => contact.id.toString() !== contactId,
+    );
 
     await fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
-    const status = filteredContacts.length !== JSON.parse(contacts).length;
+
+    const status = filteredContacts.length !== contacts.length;
 
     return status
       ? { status: 'success', code: 200, message: 'contact deleted' }
@@ -53,17 +73,6 @@ const removeContact = async contactId => {
     console.log(error.message);
   }
 };
-
-// const contacts = await listContacts();
-//   const newContacts = contacts.filter(contact => {
-//     return contact.id !== Number(contactId);
-//   });
-//   await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
-//   return newContacts;
-// } catch (error) {
-//   console.log(error.message);
-//   return;
-// }
 
 const updateContact = async (contactId, body) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,15 +4,14 @@ const model = require('../../model/index');
 const { nanoid } = require('nanoid');
 
 // GET
-router.get('/', async (req, res, next) => {
+router.get('/', async (_, res, next) => {
   try {
-    const response = await model.listContacts();
-    const data = JSON.parse(response);
+    const { code, status, contacts } = await model.listContacts();
 
-    res.status(200).json({
-      status: 'success',
-      code: 200,
-      data: { data },
+    res.status(code).json({
+      status,
+      code,
+      contacts,
     });
   } catch (error) {
     next(error);
@@ -24,28 +23,14 @@ router.get('/', async (req, res, next) => {
 router.get('/:contactId', async (req, res, next) => {
   const id = req.params.contactId;
   try {
-    const data = await model.getContactById(id);
+    const { code, status, contact, message } = await model.getContactById(id);
 
-    // const { code, status, message } = await model.removeContact(id);
-
-    // res.status(code).json({
-    //   status,
-    //   code,
-    //   message,
-    // });
-    /* eslint-disable */
-    data
-      ? res.status(200).json({
-          status: 'success',
-          code: 200,
-          data: { data },
-        })
-      : res.status(404).json({
-          status: 'error',
-          code: 404,
-          message: 'Not found',
-        });
-    /* eslint-enable */
+    res.status(code).json({
+      status,
+      code,
+      message,
+      contact,
+    });
   } catch (error) {
     next(error);
   }
@@ -55,21 +40,21 @@ router.get('/:contactId', async (req, res, next) => {
 // POST
 router.post('/', async (req, res, next) => {
   const { name, email, phone } = req.body;
-
   const body = {
     id: nanoid(),
-    name: name,
-    email: email,
-    phone: phone,
+    name,
+    email,
+    phone,
   };
 
   try {
-    await model.addContact(body);
+    const { code, status, contact, message } = await model.addContact(body);
 
-    res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: { body },
+    res.status(code).json({
+      status,
+      code,
+      contact,
+      message,
     });
   } catch (error) {
     next(error);
@@ -93,26 +78,12 @@ router.delete('/:contactId', async (req, res, next) => {
   }
   res.end();
 });
-// res.send("it's delete:contactId");
-// res.json({ message: 'template message' });
-/*
-try {
-  const contacts = await listContacts();
-  const newContacts = contacts.filter(contact => {
-    return contact.id !== Number(contactId);
-  });
-  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
-  return newContacts;
-} catch (error) {
-  console.log(error.message);
-  return;
-}
-*/
 
-// router.patch('/:contactId', async (req, res, next) => {
-//   res.send("it's patch:contactId");
-//   res.json({ message: 'template message' });
-// });
+// PATCH
+router.patch('/:contactId', async (req, res, next) => {
+  res.send("it's patch:contactId");
+  res.json({ message: 'template message' });
+});
 
 // ====
 // router.get('/error', function (req, res) {
