42415471c9f7dec84464073a45d3646ad27c6560
Serhii Stanislav <serhiistanislav@gmail.com>
2021-05-14 12:16:51 +0300

    Done!

--- bin/server.js
+++ bin/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 db.then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
+    console.log(`Database connection successful. PORT: ${PORT}`);
   });
 }).catch(err => {
   console.log(`Server not run. Error: ${err.message}`);
