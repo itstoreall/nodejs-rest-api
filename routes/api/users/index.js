11b6e8e5a2a9d06d27f89985eaf12045ff93a0ed
Serhii Stanislav <serhiistanislav@gmail.com>
2021-05-20 18:40:04 +0300

    adds a Loguot

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../../controllers/users.js');
+const guard = require('../../../helpers/guard');
 
 router.post('/register', ctrl.reg); // return jwt-token
 router.post('/login', ctrl.login);
-router.post('/logout', ctrl.logout);
+router.post('/logout', guard, ctrl.logout); // Logout only through guard
 
 module.exports = router;
