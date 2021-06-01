6000e3c5e7c8c662c96f65724b7a4baaee4d1889
Serhii Stanislav <serhiistanislav@gmail.com>
2021-06-01 20:37:46 +0300

    adds a multer

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,18 @@
 const app = require('../app');
 const db = require('../model/db');
+const createFolderIsNotExist = require('../helpers/create_dir');
+
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+const UPLOAD_DIR = process.env.UPLOAD_DIR;
+const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
 
+// Starts the SERVER
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    await createFolderIsNotExist(UPLOAD_DIR);
+    await createFolderIsNotExist(AVATARS_OF_USERS);
     console.log(`Database connection successful. PORT: ${PORT}`);
   });
 }).catch(err => {
