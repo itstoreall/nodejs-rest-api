1ffc4ed41de183b751d4eeae40267b2038448d13
Serhii Stanislav <serhiistanislav@gmail.com>
2021-06-04 12:59:47 +0300

    adds the Hooks to the E2E test

---
+++ test/data/data.js
@@ -0,0 +1,20 @@
+const newContact = {
+  name: 'Vova',
+  phone: '0679994455',
+};
+
+const newUser = {
+  name: 'Alex',
+  gender: 'mail',
+  email: 'alex@mail.com',
+  password: '123456',
+};
+
+const newTestUser = {
+  name: 'Alex',
+  gender: 'mail',
+  email: 'new_alex_user@mail.com',
+  password: '123456',
+};
+
+module.exports = { newUser, newContact, newTestUser };
